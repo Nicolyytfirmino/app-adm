f0e6d74d1ec80007164b88f32104fa1dbb75b94f
NICOLY YASMIM TORRES FIRMINO <nicoly.ytfirmino@senacsp.edu.br>
2018-03-29 22:16:54 -0300

    lista contatos e orcamento at

--- www/js/lista-orcamento.js
+++ www/js/lista-orcamento.js
@@ -8,13 +8,12 @@
         
             snapshot.forEach(function(item){
                   var listHtml = '<div class="row block block-strong">';
-                  listHtml += '<div class="col-20">'+ item.val().nome +'</div>';
+                  listHtml += '<div class="col-5">'+ item.val().nome +'</div>';
                     listHtml += '<div class="col-5">'+ item.val().email +'</div>';
                     listHtml += '<div class="col-5">'+ item.val().endereco +'</div>';
                     listHtml += '<div class="col-5">'+ item.val().bairro +'</div>';
                     listHtml += '<div class="col-5">'+ item.val().cidade +'</div>';
                     listHtml += '<div class="col-5">'+ item.val().estado +'</div>';
-                    listHtml += '<div class="col-5">'+ item.val().cidade +'</div>';
                     listHtml += '<div class="col-5">'+ item.val().telefone +'</div>';
                     listHtml += '<div class="col-5">'+ item.val().finalidade +'</div>';
                     listHtml += '<div class="col-5">'+ item.val().comodo +'</div>';
